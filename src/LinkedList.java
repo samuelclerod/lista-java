0139f905104fdecc0aebc71ada60a38787611eab
Samuel Rodrigues <samuelclerod@gmail.com>
2022-10-31 20:03:28 -0300

    feat: update, show, size

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,47 +1,81 @@
 
-public class LinkedList implements List{
-	
+public class LinkedList implements List {
+
 	private Node head;
 
 	@Override
 	public void add(int value) {
-		
+
 		Node newNode = new Node(value);
-		
-		if(head==null) {
+
+		if (head == null) {
 			head = newNode;
 		} else {
 			Node current = head;
-			while(current.next==null) {
+			while (current.next != null) {
 				current = current.next;
 			}
-			
+
 			current.next = newNode;
 		}
-		
+
 	}
 
 	@Override
 	public void remove(int value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public String show() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.head == null) {
+			return "lista vazia";
+		}
+
+		String out = "";
+		Node current = this.head;
+		while (current != null) {
+			out += current.content + ' ';
+			current = current.next;
+		}
+		return out;
 	}
 
 	@Override
 	public void update(int position, int value) {
-		// TODO Auto-generated method stub
-		
+		if(this.head==null) {
+			return;
+		}
+		if (position < 1 || position > this.size()) {
+			return;
+		}
+
+		int count = 1;
+
+		Node i = this.head;
+		while (i != null) {
+			
+			if(count==position) {
+				i.content = value;
+				break;
+			}
+			
+			count++;
+			i = i.next;
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+
+		Node i = this.head;
+		while (i != null) {
+			count++;
+			i = i.next;
+		}
+
+		return count;
 	}
 }
